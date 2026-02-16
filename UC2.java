0a7850be7ede99ea3133ce222b008f0c7b13aa6e
JeevanPranav06 <jeevanpranav789@gmail.com>
2026-02-16 09:10:28 +0530

    v4

--- UC2.java
+++ UC2.java
@@ -1,10 +1,18 @@
 public class UC2 {
     public static void main(String[] args) {
-        System.out.println("   ____  ____  ____  _____\n" +
-                "  / __ \\/ __ \\/ __ \\/ ___/\n" +
-                " / / / / / / / /_/ /\\__ \\ \n" +
-                "/ /_/ / /_/ / ____/___/ / \n" +
-                "\\____/\\____/_/    /____/  \n" +
-                "                          ");
+        System.out.println("                                      ");
+        System.out.println("  .####.    .####.   ######:    :####:");
+        System.out.println("  ######    ######   #######:  :######");
+        System.out.println(" :##  ##:  :##  ##:  ##   :##  ##:  :#");
+        System.out.println(" ##:  :##  ##:  :##  ##    ##  ##     ");
+        System.out.println(" ##    ##  ##    ##  ##   :##  ###:   ");
+        System.out.println(" ##    ##  ##    ##  #######:  :#####:");
+        System.out.println(" ##    ##  ##    ##  ######:    .#####");
+        System.out.println(" ##    ##  ##    ##  ##            :##");
+        System.out.println(" ##:  :##  ##:  :##  ##              #");
+        System.out.println(" :##  ##:  :##  ##:  ##        #:.  :#");
+        System.out.println("  ######    ######   ##        #######");
+        System.out.println("  .####.    .####.   ##        .#####:");
+        System.out.println("                                      ");
     }
 }
