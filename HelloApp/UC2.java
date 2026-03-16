44272e500827eefdd6e8565fc707c2205994ea8d
JeevanPranav06 <jeevanpranav789@gmail.com>
2026-03-17 01:03:23 +0530

    Hello App

---
+++ HelloApp/UC2.java
@@ -0,0 +1,12 @@
+package HelloApp;
+
+public class UC2 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String name = args[0];
+            System.out.println("Hello, " + name + "!");
+        }
+    }
+}
