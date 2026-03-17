e0c676e16d59da772d987fb1d4a398edeb6bddf7
JeevanPranav06 <jeevanpranav789@gmail.com>
2026-03-17 09:14:48 +0530

    Hello App UC3 and UC4

---
+++ HelloApp/UC4.java
@@ -0,0 +1,12 @@
+package HelloApp;
+
+public class UC4 {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
+        } else {
+            String names = String.join(", ", args);
+            System.out.println("Hello, " + names + "!");
+        }
+    }
+}
